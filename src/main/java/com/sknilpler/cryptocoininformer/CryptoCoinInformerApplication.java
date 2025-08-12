789aac30e04a3a24daadc062c5b3c177d4ecb09c
sknilpler <nesterov.craft.29@gmail.com>
2025-08-12 23:59:08 +0500

    Currency pairs added

--- src/main/java/com/sknilpler/cryptocoininformer/CryptoCoinInformerApplication.java
+++ src/main/java/com/sknilpler/cryptocoininformer/CryptoCoinInformerApplication.java
@@ -9,6 +9,7 @@ public class CryptoCoinInformerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoCoinInformerApplication.class, args);
 		System.out.println("To connect to swagger: http://[ip]:[port]/swagger-ui/index.html");
+		System.out.println("http://localhost:8080/swagger-ui/index.html");
 	}
 
 }
--- src/main/java/com/sknilpler/cryptocoininformer/util/FormattingSymbol.java
+++ src/main/java/com/sknilpler/cryptocoininformer/util/FormattingSymbol.java
@@ -8,16 +8,15 @@ public class FormattingSymbol {
     // Список известных базовых валют
     private static final Set<String> baseCurrencies = new HashSet<>(Arrays.asList(
             "BTC", "ETH", "USDT", "BNB", "LTC", "XRP", "DOGE", "ADA", "DOT", "SOL", "SHIB", "TRX", "MATIC",
-            "BCH", "XLM", "LINK", "ATOM", "UNI", "FIL", "ALGO", "VET", "FTM", "AVAX", "EGLD", "XTZ",
+            "BCH", "XLM", "LINK", "ATOM", "UNI", "FIL", "ALGO", "VET", "FTM", "AVAX", "EGLD", "XTZ", "FDUSD",
             "NEAR", "AAVE", "GRT", "SAND", "MANA", "THETA", "AXS", "KSM", "ZEC", "DASH", "COMP", "CRV",
             "SNX", "RUNE", "ENJ", "KAVA", "MKR", "YFI", "1INCH", "BAT", "CHZ", "HNT", "ICX", "ONT",
-            "ZIL", "IOST", "WAVES", "QTUM", "ZRX", "LUNA", "CAKE", "XEC", "GALA", "CELO", "KLAY",
-            "XNO", "LRC", "BTT", "STMX", "ANKR", "ROSE", "JASMY", "RSR", "KNC", "GNO", "ENS", "OMG",
-            "FLOW", "INJ", "CRO", "RVN", "CSPR", "SUSHI", "XVG", "TWT", "CELR", "QNT", "DYDX", "NKN",
-            "DGB", "LSK", "REP", "POLY", "STORJ", "RAY", "MINA", "GTC", "PYR", "SPELL", "LPT", "KEEP",
-            "SRM", "OCEAN", "UMA", "BNT", "BAL", "ORN", "MTL", "KSM", "SKL", "REEF", "CTSI", "DENT"
+            "ZIL", "IOST", "WAVES", "QTUM", "ZRX", "LUNA", "CAKE", "XEC", "GALA", "CELO", "KLAY", "XAI",
+            "XNO", "LRC", "BTT", "STMX", "ANKR", "ROSE", "JASMY", "RSR", "KNC", "GNO", "ENS", "OMG", "WIF", "DYM",
+            "FLOW", "INJ", "CRO", "RVN", "CSPR", "SUSHI", "XVG", "TWT", "CELR", "QNT", "DYDX", "NKN", "BUSD",
+            "DGB", "LSK", "REP", "POLY", "STORJ", "RAY", "MINA", "GTC", "PYR", "SPELL", "LPT", "KEEP", "ERD",
+            "SRM", "OCEAN", "UMA", "BNT", "BAL", "ORN", "MTL", "KSM", "SKL", "REEF", "CTSI", "DENT", "GBP", "PHB"
     ));
-
     // Метод для приведения символа в формат "BASE-QUOTE"
     public static String formatSymbol(String symbol) {
         // Если символ содержит разделители (_ / -), используем стандартный метод разделения
@@ -30,10 +29,15 @@ public class FormattingSymbol {
 
         // Если символ слитно (например, "BTCUSDT"), разделяем по известным базовым валютам
         for (String base : baseCurrencies) {
+            // Если базовая валюта находится в начале
             if (symbol.startsWith(base.toUpperCase())) {
-                // Отделяем базовую валюту и оставшуюся котируемую валюту
                 String quote = symbol.substring(base.length());
                 return base.toUpperCase() + "-" + quote.toUpperCase();
+            }
+            // Если базовая валюта находится в конце
+            if (symbol.endsWith(base.toUpperCase())) {
+                String quote = symbol.substring(0, symbol.length() - base.length());
+                return quote.toUpperCase() + "-" + base.toUpperCase();
             }
         }
 
